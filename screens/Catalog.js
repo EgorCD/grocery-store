7913877bf2740666b6cf8585d067a3cb365c2c44
chumakov_ed <egorcd2004@gmail.com>
2024-10-27 22:34:50 +0200

    feat: add screen components for tabs Home, Catalog, Addresses, Statistics, and Profile

---
+++ screens/Catalog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Catalog() {
+  return (
+    <View>
+      <Text>Catalog Screen</Text>
+    </View>
+  );
+}
+
+export default Catalog;
