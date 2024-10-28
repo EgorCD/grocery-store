104753ff83ea7958b6fd9ab1186183903588a470
chumakov_ed <egorcd2004@gmail.com>
2024-10-28 20:38:06 +0200

    feat: integrate onAuthStateChanged in App.js for authentication

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import AuthStackNavigator from './navigation/AuthStackNavigator';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { app } from './firebaseConfig';
 
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const removeAuthListener = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+    });
+
+    return () => removeAuthListener();
+  }, [auth]);
+
   return (
     <NavigationContainer>
-      <BottomTabNavigator />
+      {isAuthenticated ? <BottomTabNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
 }
