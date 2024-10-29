4cb978e48a07ba837f998d4a6bc9c30ff6bde785
chumakov_ed <egorcd2004@gmail.com>
2024-10-29 13:58:05 +0200

    feat: integrate Firestore to map user properties to current user UID in AuthService

--- services/AuthService.js
+++ services/AuthService.js
@@ -1,12 +1,24 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getFirestore, doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 import { app } from '../firebaseConfig';
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export const signUp = async (email, password) => {
+export const signUp = async (email, password, additionalData) => {
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        return userCredential.user;
+        const user = userCredential.user;
+
+        await setDoc(doc(db, "users", user.uid), {
+            email: user.email,
+            createdAt: new Date(),
+            ...additionalData,
+        });
+
+        console.log("User added to Firestore");
+
+        return user;
     } catch (error) {
         throw error;
     }
@@ -26,5 +38,28 @@ export const logOut = async () => {
         return await signOut(auth);
     } catch (error) {
         throw error;
+    }
+};
+
+export const getUserProfile = async (uid) => {
+    try {
+        const userDoc = await getDoc(doc(db, "users", uid));
+        if (userDoc.exists()) {
+            return userDoc.data();
+        } else {
+            throw new Error("User profile not found");
+        }
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const updateUserProfile = async ({ uid, name }) => {
+    try {
+        const userDocRef = doc(db, "users", uid);
+        await updateDoc(userDocRef, { name });
+    } catch (error) {
+        console.error("Error updating user profile:", error);
+        throw new Error("Failed to update user profile");
     }
 };
