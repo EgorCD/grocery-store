15fb23f3a0a96ad35fab976f60fd9eb8a8c18fb4
chumakov_ed <egorcd2004@gmail.com>
2024-10-29 13:54:07 +0200

    chore: update Statistics screen with styling improvements

--- screens/Statistics.js
+++ screens/Statistics.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 function Statistics() {
-  return (
-    <View>
-      <Text>Statistics Screen</Text>
-    </View>
-  );
+    return (
+        <View>
+            <Text>Stafftistics Screen</Text>
+        </View>
+    );
 }
 
 export default Statistics;
