919a3be247c042435e718fde4947ee8c62050d07
chumakov_ed <egorcd2004@gmail.com>
2024-11-01 10:28:02 +0200

    feat: add categoryOptions for dynamic category rendering in Addresses

---
+++ data/categoryOptions.js
@@ -0,0 +1,12 @@
+const categoryOptions = [
+    { 
+        id: 1, 
+        name: 'Coffee' 
+    },
+    { 
+        id: 2, 
+        name: 'Grocery' 
+    },
+];
+
+export default categoryOptions;
