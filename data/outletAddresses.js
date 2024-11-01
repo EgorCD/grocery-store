fe77aa45a0d44be1ca504e991e58b52b1ea1da54
chumakov_ed <egorcd2004@gmail.com>
2024-11-01 09:32:03 +0200

    feat: move outlet addresses to separate file in a data folder

---
+++ data/outletAddresses.js
@@ -0,0 +1,32 @@
+const outletAddresses = [
+    {
+        name: 'Coffee Shop',
+        category: 'Coffee',
+        address: 'Aleksanterinkatu 52, Helsinki',
+        latitude: 60.1699,
+        longitude: 24.9384
+    },
+    {
+        name: 'Coffee Shop',
+        category: 'Coffee',
+        address: 'Mannerheimintie 20, Helsinki',
+        latitude: 60.1700,
+        longitude: 24.9390
+    },
+    {
+        name: 'Grocery Store',
+        category: 'Grocery',
+        address: 'Kaivokatu 8, Helsinki',
+        latitude: 60.1710,
+        longitude: 24.9380
+    },
+    {
+        name: 'Grocery Store',
+        category: 'Grocery',
+        address: 'Fredrikinkatu 47, Helsinki',
+        latitude: 60.1720,
+        longitude: 24.9370
+    },
+];
+
+export default outletAddresses;
