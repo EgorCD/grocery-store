79705f329dac74f0fcdfb6d3e4a203b58a352a59
chumakov_ed <egorcd2004@gmail.com>
2024-11-01 09:32:23 +0200

    refactor: import outlet addresses in Addresses.js

--- screens/Addresses.js
+++ screens/Addresses.js
@@ -2,41 +2,10 @@ import React, { useState } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import Map from '../components/Map';
 import { COMMON_STYLES, COLORS, SPACING } from '../styles/styles';
+import outletAddresses from '../data/outletAddresses';
 
 function Addresses() {
     const [selectedCategory, setSelectedCategory] = useState('Coffee');
-
-    const outletAddresses = [
-        {
-            name: 'Coffee Shop',
-            category: 'Coffee',
-            address: 'Aleksanterinkatu 52, Helsinki',
-            latitude: 60.1699,
-            longitude: 24.9384
-        },
-        {
-            name: 'Coffee Shop',
-            category: 'Coffee',
-            address: 'Mannerheimintie 20, Helsinki',
-            latitude: 60.1700,
-            longitude: 24.9390
-        },
-        {
-            name: 'Grocery Store',
-            category: 'Grocery',
-            address: 'Kaivokatu 8, Helsinki',
-            latitude: 60.1710,
-            longitude: 24.9380
-        },
-        {
-            name: 'Grocery Store',
-            category: 'Grocery',
-            address: 'Fredrikinkatu 47, Helsinki',
-            latitude: 60.1720,
-            longitude: 24.9370
-        },
-    ];
-
     const filteredAddresses = outletAddresses.filter(location => location.category === selectedCategory);
 
     return (
