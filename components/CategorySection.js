ab759566caf94b268271aef9f9b6ffcc33a75e46
chumakov_ed <egorcd2004@gmail.com>
2024-11-04 12:31:27 +0200

    feat: add CategorySection component for displaying categorized item lists

---
+++ components/CategorySection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { COMMON_STYLES, SPACING } from '../styles/styles';
+import ItemList from './ItemList';
+
+const CategorySection = ({ categoryName }) => {
+    return (
+        <View style={{ marginTop: SPACING.medium }}>
+            <Text style={COMMON_STYLES.categoryTitleText}>{categoryName}</Text>
+            <ItemList category={categoryName.toLowerCase()} />
+        </View>
+    );
+};
+
+export default CategorySection;
