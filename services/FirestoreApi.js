5f9aa6706f23a87b339ba13a54c1ea79e0930454
chumakov_ed <egorcd2004@gmail.com>
2024-11-08 10:55:56 +0200

    feat: implement stock update function in FirestoreApi for order functionality

--- services/FirestoreApi.js
+++ services/FirestoreApi.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, runTransaction } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import { app } from "../firebaseConfig";
 
@@ -16,6 +16,32 @@ export const fetchItems = async (category) => {
         return items;
     } catch (error) {
         console.error(`Error fetching ${category} items:`, error);
+        throw error;
+    }
+};
+
+// https://firebase.google.com/docs/firestore/manage-data/transactions
+
+export const updateStock = async (selectedItems) => {
+    try {
+        await runTransaction(db, async (transaction) => {
+            for (const item of selectedItems) {
+                const itemRef = doc(db, 'products', item.category, 'items', item.id);
+                const itemDoc = await transaction.get(itemRef);
+                
+                if (!itemDoc.exists()) {
+                    throw new Error(`Item "${item.name}" does not exist.`);
+                }
+                const currentStock = itemDoc.data().stock;
+
+                if (currentStock < item.selectedQuantity) {
+                    throw new Error(`Insufficient stock for "${item.name}".`);
+                }
+                transaction.update(itemRef, { stock: currentStock - item.selectedQuantity });
+            }
+        });
+    } catch (error) {
+        console.error('Error updating stock:', error);
         throw error;
     }
 };
