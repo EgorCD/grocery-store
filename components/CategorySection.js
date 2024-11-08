14340b588f3770828a0244b8ceabd28e199f096a
chumakov_ed <egorcd2004@gmail.com>
2024-11-08 11:36:07 +0200

    refactor: remove unused spacing prop from common styles

--- components/CategorySection.js
+++ components/CategorySection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { COMMON_STYLES, SPACING } from '../styles/styles';
+import { COMMON_STYLES } from '../styles/styles';
 import ItemList from './ItemList';
 
 const CategorySection = ({ categoryName }) => {
