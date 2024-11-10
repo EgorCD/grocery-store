a11888fe6e7a303376211f095931e5066c6111cb
chumakov_ed <egorcd2004@gmail.com>
2024-11-10 15:14:23 +0200

    feat: add OrderButton component for ordering items

---
+++ components/OrderButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TouchableOpacity, Text, View } from 'react-native';
+import { COMMON_STYLES, SPACING } from '../styles/styles';
+
+const OrderButton = ({ onPress }) => (
+    <View style={{ marginRight: SPACING.small, marginLeft: SPACING.small }}>
+        <TouchableOpacity
+            style={[COMMON_STYLES.primaryButton, { marginTop: SPACING.medium }]}
+            onPress={onPress}
+        >
+            <Text style={COMMON_STYLES.primaryButtonText}>Order</Text>
+        </TouchableOpacity>
+    </View>
+);
+
+export default OrderButton;
