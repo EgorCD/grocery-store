d69d98cebeeef3d7709f7a5d2c7f5854d37a0774
Egor Chumakov <135476699+EgorCD@users.noreply.github.com>
2024-11-27 23:04:11 +0200

    feat: make Home screen scrollable using ScrollView

--- screens/Main/Home.js
+++ screens/Main/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView} from 'react-native';
 import { COMMON_STYLES } from '../../styles/styles';
 import CategorySection from '../../components/items/CategorySection';
 import UserGreeting from '../../components/UserGreeting';
 
 function Home() {
   return (
-    <View style={COMMON_STYLES.outerContainer}>
+    <ScrollView style={COMMON_STYLES.outerContainer}>
       <UserGreeting />
       <CategorySection categoryName="Coffee" />
       <CategorySection categoryName="Grocery" />
-    </View>
+    </ScrollView>
   );
 }
 
